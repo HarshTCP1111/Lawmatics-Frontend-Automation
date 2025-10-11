ef9e48e6c71693eca4e0158c60a3cc93b94d3cf2
Harsh Shuddhalwar <80185357+h1102sh-Developer@users.noreply.github.com>
2025-10-11 22:07:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { HashRouter as Router } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import ProtectedRoute from './components/ProtectedRoute';
